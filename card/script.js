2d98e089d2619ee62250008d436acf67eb333b26
ezcodeweb <147483544+ezcodeweb@users.noreply.github.com>
2024-01-24 09:24:30 +0000

    changes

--- card/script.js
+++ card/script.js
@@ -21,13 +21,30 @@ function kt(str) {
     let count = 0;
     str = str.replace(/\s/g, '');
     for (let i = 0; i < str.length; i++) {
-        if (str[i] === "J" || str[i] === "Q" || str[i] === "K") {
+        if (str[i] === "A") {
+            count += 11;
+        } else if (str[i] === "J" || str[i] === "Q" || str[i] === "K") {
             count += 10;
         } else {
             count += parseInt(str[i]);
         }
     }
     return count;
+}
+
+function kt2(str) {
+    str = str.replace(/\s/g, '');
+    for (let i = 0; i < str.length; i++) {
+        if (str.length === 2) {
+            if(str[0] === "A" && kt(str[1]) === 10) {
+                return "ace"; 
+            } else if(str[0] === "A" && str[1] === "A") {
+                return "special";
+            } else {
+                return "none";
+            }
+        }
+    }
 }
 
 let b = random(1) + " ";
@@ -55,12 +72,12 @@ function hope(length) {
 let c = random(1) + " " + random(1) + " ";
 
 function ai() {
-    if(kt(c)<=15){
+    while (kt(c)<=15){
         c += random(1) + " ";
     }
-    if(kt(c)>=18){
+    if(kt(c)>=16){
         let d = hope(1);
-        if(d===1){
+        if(d === "1"){
             c += random(1);
         }
     }
@@ -70,9 +87,40 @@ function ai() {
 kq.addEventListener("click", () => {
     let diem = kt(b);
     let diem2 = kt(c);
-    if(kt(c)>21 && kt(b) < 22 && kt(b) > 15){
+    let win = kt2(c);
+    let win2 = kt2(b);
+    if(win === "ace" && win2 === "ace") {
+        ai();
+        document.getElementById("note").innerHTML = "Draw";
+    } else if (win === "special" && win2 === "ace") {
+        ai();
+        document.getElementById("note").innerHTML = "You lose";
+    } else if (win === "special" && win2 === "special") {
+        ai();
+        document.getElementById("note").innerHTML = "Draw";
+    } else if (win === "ace" && win2 === "special") {
+        ai();
+        document.getElementById("note").innerHTML = "You win";
+    } else if (win === "special" && win2 === "ace") {
+        ai();
+        document.getElementById("note").innerHTML = "You lose";
+    } else if (win === "special" && win2 === "none") {
+        ai();
+        document.getElementById("note").innerHTML = "You lose";
+    } else if (win === "ace" && win2 === "none") {
+        ai();
+        document.getElementById("note").innerHTML = "You lose";
+    } else if (win === "none" && win2 === "ace") {
+        ai();
+        document.getElementById("note").innerHTML = "You win";
+    } else if (win === "none" && win2 === "special") {
+        ai();
+        document.getElementById("note").innerHTML = "You win";
+    } else if(kt(c)>21 && kt(b) < 22 && kt(b) > 15){
+        ai();
         document.getElementById("note").innerHTML = "You win";
     } else if(kt(c)===kt(b)){
+        ai();
         document.getElementById("note").innerHTML = "Draw";
     } else if (diem > 21 || diem < 16 || diem < diem2) {
         ai();
